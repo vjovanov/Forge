469e4d851d4740a2692269099d340314dfa1a3c1
Christopher Aberger <christopheraberger@dnab404684.stanford.edu>
2014-05-29 13:49:42 -0700

    Made undirected triangle counting nice.

--- apps/OptiGraph/src/UndirectedTriangleCounting.scala
+++ apps/OptiGraph/src/UndirectedTriangleCounting.scala
@@ -14,22 +14,14 @@ trait UndirectedTriangleCounting extends OptiGraphApplication {
 
     if (args.length < 1) printUsage
 
-    //Works for both directed and undirected, performance 
-    val underForHash = 2//args(1).toInt
-    //println("Under for hash: " + underForHash)
-    val bitSetMultiplier = 32
-
     tic("input loading")
     val edgeList = loadUndirectedEdgeList(args(0))
     toc("input loading",edgeList)
+
     tic("creating graph",edgeList)
-    //val g = habPrunedUndirectedGraphFromEdgeList(loadUndirectedEdgeList(args(0)),underForHash,bitSetMultiplier)
     val g = csrUndirectedGraphFromEdgeList(edgeList)
-    //val g = csrPrunedUndirectedGraphFromEdgeList(createMeshEdgeList(5000))
     toc("creating graph",g)
     
-
-    //val g = loadUndirectedAdjEdgeList(args(0))
 
     println("Directed: " + g.isDirected)
     println("Number of Nodes: " + g.numNodes)
