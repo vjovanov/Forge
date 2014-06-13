fccd4d2478434d52c5e438293f1435a447d6e4fc
Christopher De Sa <cdesa@stanford.edu>
2014-06-13 04:26:21 -0700

    Working on app

--- apps/OptiML/src/MatrixCompletion.scala
+++ apps/OptiML/src/MatrixCompletion.scala
@@ -7,24 +7,30 @@ object MatrixCompletionInterpreter extends OptiMLApplicationInterpreter with Mat
 
 trait MatrixCompletion extends OptiMLApplication {
   def printUsage = {
-    println("Usage: MatrixCompletion <input data file> <nrows> <ncols>")
+    println("Usage: MatrixCompletion <input data file> <size>")
     exit(-1)
   }
 
   def main() = {
-    if (args.length != 3) printUsage
+    if (args.length != 2) printUsage
 
-    val nr = args(2).toInt
-    var nc = args(3).toInt
+    val n = args(2).toInt
 
-    val mBuilder = SparseMatrix[Double](nr, nc)
-    var tmpx = readVector[DenseVector[String]](args(1), { line => 
+    val mBuilder = SparseMatrix[Double](n, n)
+    var tmpx = readVector[Unit](args(1), { line => 
       mBuilder(line(0).toInt, line(1).toInt) = line(2).toDouble 
     }, " ")
     val a = mBuilder.finish
 
-    println(nr)
-    println(nc)
-    println(a)
+    tic()
+    val y0 = DenseVector.ones(n)
+
+    val alpha = 1.0
+
+    val w = untilconverged(y0, maxIter = 30) { y =>
+      (1 - alpha) * y + A * y * (y *:* y)^(-2)
+    }
+
+    toc(w)
   }
 }
