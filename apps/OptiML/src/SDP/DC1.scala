264ab482e233df417df6dda2ada016f7858e6eb8
Christopher De Sa <cdesa@stanford.edu>
2014-07-17 15:34:26 -0700

    step

--- apps/OptiML/src/SDP/DC1.scala
+++ apps/OptiML/src/SDP/DC1.scala
@@ -38,7 +38,7 @@ trait DC1 extends OptiMLApplication {
 
       val y = y0
       val ak = alpha
-      val y_next = y + a * y
+      val y_next = alpha * y // + a * y
     //  val y_next =  + a * y
 
     //   pack(y_next, k + 1)
